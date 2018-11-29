14ef22cb9282ce3c9a0561334e78447ef7f8dd20
RCVZ <robinvz17@gmail.com>
2018-11-29 21:52:41 +0100

    Changed navigation

--- src/Components/Playlists/Playlists.js
+++ src/Components/Playlists/Playlists.js
@@ -6,26 +6,37 @@ class Playlists extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userPlaylists: []
+      userPlaylists: [],
+      results: 3
     }
   }
 
   componentDidMount() {
     SpotifyApi.getPlaylist().then( Playlists => {
-      console.log('test',Playlists);
-      this.setState({
-        userPlaylists: Playlists
-      })
+      this.setState({ userPlaylists: Playlists })
     })
   }
 
+  handleOnClick = (e) => {
+    this.state.results === 3 ? this.setState({results: 6}) :  this.setState({results: 3});
+  }
+
   render() {
+    const { results } = this.state;
     return(
       <div className='Playlists'>
-        <h3>Playlists</h3>
-        {this.state.userPlaylists.map(playlist => {
-          return <h4 key={playlist.id}>{playlist.name}</h4>
-        })}
+        <h3>New</h3>
+        <h3>Search</h3>
+        <h3>Library</h3>
+        <div>
+          <h3 onClick={this.handleOnClick} >Playlists</h3>
+          {this.state.userPlaylists
+            .slice(0,results)
+            .map(playlist => {
+              return <h4 key={playlist.id}>{playlist.name}</h4>
+            })}
+
+        </div>
       </div>
     );
   }
