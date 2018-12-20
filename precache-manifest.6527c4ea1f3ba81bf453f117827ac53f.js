10a6e7f6f693be77311341becbcfd573dfa66016
RCVZ <robinvz17@gmail.com>
2018-12-20 21:42:40 +0100

    Updates

---
+++ precache-manifest.6527c4ea1f3ba81bf453f117827ac53f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c381d412b2a9ffecec51",
+    "url": "/SpotifyClone/static/js/runtime~main.c381d412.js"
+  },
+  {
+    "revision": "7081dde0ec1a75795d17",
+    "url": "/SpotifyClone/static/js/main.7081dde0.chunk.js"
+  },
+  {
+    "revision": "c14222c7bb36056f6729",
+    "url": "/SpotifyClone/static/js/1.c14222c7.chunk.js"
+  },
+  {
+    "revision": "7081dde0ec1a75795d17",
+    "url": "/SpotifyClone/static/css/main.05d76894.chunk.css"
+  },
+  {
+    "revision": "1d6e6d4c1942b83ece115cc95e47427a",
+    "url": "/SpotifyClone/index.html"
+  }
+];
