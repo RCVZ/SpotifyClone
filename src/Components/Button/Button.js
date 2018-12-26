1bd07ad882057e179425f69c464a5b9fc7f531dc
RCVZ <robinvz17@gmail.com>
2018-12-26 16:24:41 +0100

    Changed color button

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -2,14 +2,12 @@ import React, { PureComponent } from 'react';
 import './Button.css';
 
 
-class Button extends PureComponent {
-  render() {
-    return(
-      <React.Fragment>
-        <button className="Button" type={this.props.type} onClick={this.props.onClick} >{this.props.name}</button>
-      </React.Fragment>
-    );
-  }
+const Button = ({ type, onClick, name}) => {
+  return (
+    <React.Fragment>
+      <button className="Button" type={type} onClick={onClick} >{name}</button>
+    </React.Fragment>
+  );
 }
 
 export default Button;
