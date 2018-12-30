ba7c1995a5b53fe2191373ae6ae06b6e75e2ae1d
RCVZ <robinvz17@gmail.com>
2018-12-30 16:39:45 +0100

    refactored Card component

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,47 +4,25 @@ import './Card.css';
 import Text from '../Text/Text';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import { faPlay, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 
-class Card extends PureComponent {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      visibility: 'hidden'
-    }
-  }
-
-  handleMouseEnter = (e) => {
-    this.setState({ visibility: 'visible' });    
-  }
-
-  handleMouseLeave = (e) => {
-    this.setState({ visibility: 'hidden'})    
-  }
-
-  render() {
-    const { playlist, handleOnclick, id } = this.props;
-    const { visibility } = this.state;
-    return (
-      <div className="Card" onMouseLeave={this.handleMouseLeave} onMouseEnter={this.handleMouseEnter} >
-        <div className="Card-Img" onClick={() => handleOnclick(id, playlist.images)} >
-          <img src={playlist.images[0].url} alt="img" />
-          <Text name={playlist.name} />
-        </div>
-        <div className="Card-Overlay" style={{ visibility: visibility }}>
-          <div>
-            <p>.</p>
-            <p>.</p>
-            <p>.</p>
-          </div>
-          <button className="play-pause" onClick={this.onPlayClick} >
-            <FontAwesomeIcon className="button" icon={faPlay} size="lg" />
-          </button>          
-        </div>
+const Card = ({ playlist, handleOnclick, id }) => {
+  return (
+    <div className="Card">
+      <div className="Card-Img" onClick={() => handleOnclick(id, playlist.images)} >
+        <img src={playlist.images[0].url} alt="img" />
+        <Text name={playlist.name} />
       </div>
-      )
-    }
+      <div className="Action-Overlay">
+        <button className="play-pause" onClick={onPlayClick} >
+          <FontAwesomeIcon className="button" icon={faPlusCircle} size="lg" />
+        </button>
+        <button className="play-pause" onClick={onPlayClick} >
+          <FontAwesomeIcon className="button" icon={faPlay} size="lg" />
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Card;
