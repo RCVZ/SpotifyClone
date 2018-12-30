38e680ff6409250d8ee524ecc4c2d13479f5ca99
RCVZ <robinvz17@gmail.com>
2018-12-30 16:47:26 +0100

    Made component of overlay

---
+++ src/Components/ActionOverlay/ActionOverlay.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ActionOverlay.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+
+const ActionOverlay = ({ addToPlaylist, onPlayClick }) => {
+  return (
+    <div className="Action-Overlay">
+      <button className="play-pause" onClick={addToPlaylist} >
+        <FontAwesomeIcon className="button" icon={faPlusCircle} size="lg" />
+      </button>
+      <button className="play-pause" onClick={onPlayClick} >
+        <FontAwesomeIcon className="button" icon={faPlay} size="lg" />
+      </button>
+    </div>
+  )
+}
+
+export default ActionOverlay;
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,10 +1,8 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import './Card.css';
 
 import Text from '../Text/Text';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import ActionOverlay from '../ActionOverlay/ActionOverlay';
 
 const Card = ({ playlist, handleOnclick, id }) => {
   return (
@@ -13,14 +11,7 @@ const Card = ({ playlist, handleOnclick, id }) => {
         <img src={playlist.images[0].url} alt="img" />
         <Text name={playlist.name} />
       </div>
-      <div className="Action-Overlay">
-        <button className="play-pause" onClick={onPlayClick} >
-          <FontAwesomeIcon className="button" icon={faPlusCircle} size="lg" />
-        </button>
-        <button className="play-pause" onClick={onPlayClick} >
-          <FontAwesomeIcon className="button" icon={faPlay} size="lg" />
-        </button>
-      </div>
+      <ActionOverlay />
     </div>
   )
 }
