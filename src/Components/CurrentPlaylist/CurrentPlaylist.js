c41596a4afb29ed4ece1d05f40df368a1650a3ba
RCVZ <robinvz17@gmail.com>
2018-12-31 18:03:39 +0100

    restructuring

--- src/Components/CurrentPlaylist/CurrentPlaylist.js
+++ src/Components/CurrentPlaylist/CurrentPlaylist.js
@@ -2,21 +2,12 @@ import React, { PureComponent } from 'react';
 import './CurrentPlaylist.css';
 
 import TrackList from '../TrackList/TrackList';
-import Grid from '../Grid/Grid';
 
 class CurrentPlaylist extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tracks: []
-    }
-  }
   render() {
     const { playlist } = this.props;
     return (
-      <Grid>
-        <TrackList trackList={playlist} />
-      </Grid>        
+        <TrackList trackList={playlist} />    
     );
   }
 }
