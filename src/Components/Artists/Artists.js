cb05120bc4a901bc8defe14ad4a0da83cdb05356
RCVZ <robinvz17@gmail.com>
2019-01-06 21:15:43 +0100

    Added artists component

---
+++ src/Components/Artists/Artists.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import './Artists.css';
+
+import PlaylistDisplay from '../../Containers/PlaylistDisplay/PlaylistDisplay';
+import Header from '../Header/Header';
+
+import { withRouter } from "react-router-dom";
+
+const Artists = ({ artists, addToCurrentPlaylist, history }) => {
+
+  const [expand, toggleExpand] = useState({
+    expanded: false,
+    state: 'More',
+    results: 3
+  });
+
+  const handleToggleExpand = () => {
+    if (history.location.pathname === '/search/artists') {
+      history.push('/search')
+    } else {
+      history.push('/search/artists')
+    }
+  }
+
+  const results = () => {
+    return history.location.pathname === '/search/albums' ? Infinity : 3;
+  }
+
+  return (
+    <div className="Artists" >
+      <Header name={expand.state} buttonAction={handleToggleExpand}>Artists</Header>
+      <PlaylistDisplay addToCurrentPlaylist={addToCurrentPlaylist} playlists={artists.slice(0, results())} artists />
+    </div>
+  );
+}
+
+export default withRouter(Artists);
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,11 +4,11 @@ import './Card.css';
 import Text from '../Text/Text';
 import ActionOverlay from '../ActionOverlay/ActionOverlay';
 
-const Card = ({ playlist, handleOnclick, id }) => {
+const Card = ({ playlist, handleOnclick, id, artist }) => {
   return (
-    <div className="Card">
-      <div className="Card-Img" >
-        <img src={playlist.images[0].url} alt="img" />
+    <div className={`Card ${playlist.type === "artist" ? "artist" : null}`}>
+      <div className={`Card-Img ${playlist.type === "artist" ? "artist" : null}`} >
+        {playlist.images[0] ? <img src={playlist.images[0].url} alt="img" /> : null}
       </div>
       <Text name={playlist.name} />
       <ActionOverlay trackAction={() => handleOnclick(id, playlist.images)}/>
