2c838385e9c178d605f41cde1bad473f61128633
RCVZ <robinvz17@gmail.com>
2019-01-12 23:34:51 +0100

    added onPlayClick

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import './Card.css';
 
+import SpotifyApi from '../../util/Spotify';
+
 import Text from '../Text/Text';
 import ActionOverlay from '../ActionOverlay/ActionOverlay';
 
 const Card = ({ playlist, handleOnclick, id }) => {
+
+  const onPlayClick = () => {
+    const uri = playlist.uri.split(':').slice(2).join(':');
+    SpotifyApi.playTrack('', uri);
+  }
+
   return (
-    <div className={`Card ${playlist.type === "artist" ? "artist" : null}`}>
-      <div className={`Card-Img ${playlist.type === "artist" ? "artist" : null}`} >
+    <div className={`Card ${playlist.type === "artist" ? "artist" : false}`}>
+      <div className={`Card-Img ${playlist.type === "artist" ? "artist" : false}`} >
         {playlist.images[0] ? <img src={playlist.images[0].url} alt="img" /> : null}
       </div>
       <Text name={playlist.name} />
       <ActionOverlay 
         trackAction={() => handleOnclick(id, playlist)} 
         playlist={ playlist.type === "artist" ? "artist" : null }
+        onPlayClick={onPlayClick}
       />
     </div>
   )
