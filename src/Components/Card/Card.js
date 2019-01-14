9a9615d8b71e755c32c8a7db8c41988a14826fad
RCVZ <robinvz17@gmail.com>
2019-01-14 22:30:34 +0100

    added clickOnOverlay function

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import './Card.css';
 
 import SpotifyApi from '../../util/Spotify';
@@ -6,36 +6,52 @@ import SpotifyApi from '../../util/Spotify';
 import Text from '../Text/Text';
 import ActionOverlay from '../ActionOverlay/ActionOverlay';
 
-const Card = ({ playlist, handleOnclick, id }) => {
+import { ContextStore } from '../../Context/MainContext';
+
+const Card = ({ playlist, handleOnclick, id, history, istrackList }) => {
+
+  const context = useContext(ContextStore);
 
   const onPlayClick = () => {
     let uri;
+
     if (playlist.uri.includes('user')) {
       playlist.uri.split(':').splice(1, 2).join(':');
       uri = playlist.uri;
     } else {
       uri = playlist.uri.split(':').slice(2).join(':');
     }
-    console.log(playlist.uri)
-    // const uri = playlist.uri.split(':').slice(2).join(':');
-    console.log(uri)
+
     SpotifyApi.playTrack('1', uri, playlist.type);
   }
 
-  //spotify:user:1120095756:playlist:3ftnOdvI6GB3kTde5e8KgO
-  // spotify:user:1120095756:playlist:3ftnOdvI6GB3kTde5e8KgO
-   //spotify: playlist: 37i9dQZF1DWSBi5svWQ9Nk
+  const clickOnOverlay = () => {
+    if (!istrackList) {
+      return handleOnclick(playlist.id, playlist);
+    }
+    let newPlaylist =[];
+    SpotifyApi.getPlaylist(playlist.id, 'spotify').then((tracklist) => {
+      tracklist.map((playlist) => {
+        return newPlaylist.push(playlist.track);
+      })
+      console.log(newPlaylist)
+      context.updateState('tracks', newPlaylist);
+      history.push('/search/tracks')
+    })
+  }
+
 
   return (
-    <div className={`Card ${playlist.type === "artist" ? "artist" : false}`}>
-      <div className={`Card-Img ${playlist.type === "artist" ? "artist" : false}`} >
-        {playlist.images[0] ? <img src={playlist.images[0].url} alt="img" /> : null}
+    <div className={`Card ${playlist.type === "artist" ? "artist" : "playlist"}`}>
+      <div className={`Card-Img ${playlist.type === "artist" ? "artist" : "playlist"}`} >
+        {playlist.images[0] ? <img src={playlist.images[0].url} alt="img" /> : "loading"}
       </div>
       <Text name={playlist.name} />
       <ActionOverlay 
         trackAction={() => handleOnclick(id, playlist)} 
         playlist={ playlist.type === "artist" ? "artist" : null }
         onPlayClick={onPlayClick}
+        clickOnOverlay={clickOnOverlay}
       />
     </div>
   )
