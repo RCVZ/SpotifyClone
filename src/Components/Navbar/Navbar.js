570dfeeb93763d6f38716bbdb8c9ed371217ac6c
RCVZ <robinvz17@gmail.com>
2019-01-15 21:33:59 +0100

    fixed active style not showing

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import './Navbar.css';
 
 import { NavLink } from "react-router-dom";
@@ -6,16 +6,16 @@ import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faAlignJustify, faListUl, faPlusSquare, faTh } from '@fortawesome/free-solid-svg-icons';
 
-class Navbar extends PureComponent {
+class Navbar extends Component {
   render() {
     return(
       <div className='Navbar-wrapper'>
         <div className='Navbar'>
-          <NavLink className="LinkButton" to="/search"><FontAwesomeIcon icon={faSearch} />  Search</NavLink>
-          <NavLink className="LinkButton" to="/currentPlaylist"><FontAwesomeIcon icon={faAlignJustify} /> Current Playlist</NavLink>
-          <NavLink className="LinkButton" to="/newPlaylist"><FontAwesomeIcon icon={faPlusSquare} /> New Playlist</NavLink>
-          <NavLink className="LinkButton" to="/library"><FontAwesomeIcon icon={faTh} /> Library</NavLink>
-          <NavLink className="LinkButton" to="/userPlaylists"><FontAwesomeIcon icon={faListUl} /> Playlists</NavLink>
+          <NavLink to="/search"><FontAwesomeIcon icon={faSearch} />  Search</NavLink>
+          <NavLink to="/currentPlaylist"><FontAwesomeIcon icon={faAlignJustify} /> Current Playlist</NavLink>
+          <NavLink to="/newPlaylist"><FontAwesomeIcon icon={faPlusSquare} /> New Playlist</NavLink>
+          <NavLink to="/library"><FontAwesomeIcon icon={faTh} /> Library</NavLink>
+          <NavLink to="/userPlaylists"><FontAwesomeIcon icon={faListUl} /> Playlists</NavLink>
         </div>
       </div>
     );
