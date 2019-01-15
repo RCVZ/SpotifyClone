078b8e6b47ce3c60101bb04eb0b8824b0ee8d00e
RCVZ <robinvz17@gmail.com>
2019-01-15 20:44:17 +0100

    Added OpenPlaylist overlay

--- src/Components/ActionOverlay/ActionOverlay.js
+++ src/Components/ActionOverlay/ActionOverlay.js
@@ -4,14 +4,23 @@ import './ActionOverlay.css';
 import PlayButton from '../Buttons/PlayButton/PlayButton';
 import AddButton from '../Buttons/AddButton/AddButton';
 import DeleteButton from '../Buttons/DeleteButton/DeleteButton';
+import OpenButton from '../Buttons/OpenButton/OpenButton';
 
 const ActionOverlay = ({ trackAction, onPlayClick, inPlaylist, playlist, clickOnOverlay }) => {
   return (
-    <div className={`Action-Overlay ${playlist === "artist" ? "artist" : null}`} onClick={clickOnOverlay}>
+    <div className={`Action-Overlay ${playlist === "artist" ? "artist" : "playlist"}`} onClick={clickOnOverlay}>
       {inPlaylist ? <DeleteButton onDelete={trackAction} /> : <AddButton onAdd={trackAction} />}
       <PlayButton onPlayClick={onPlayClick}/>
     </div>
   )
 }
 
-export default ActionOverlay;
+export const ActionOverlayOpen = ({ onOpen }) => {
+  return (
+    <div className="Action-Overlay">
+      <OpenButton onOpen={onOpen} />
+    </div>
+  )
+}
+
+export default ActionOverlay
