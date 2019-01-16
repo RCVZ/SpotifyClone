18f53cf7a15f67cf6b61ebd802e354c78c69d05a
RCVZ <robinvz17@gmail.com>
2019-01-16 21:56:33 +0100

    added own state for Track Progression

--- src/Components/TrackProgression/TrackProgression.js
+++ src/Components/TrackProgression/TrackProgression.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './TrackProgression.css';
 
-const TrackProgression = ({ children }) => {
+import ProgressionBar from '../../Components/ProgressionBar/ProgressionBar';
+import Time from '../../Components/Time/Time';
+
+const TrackProgression = ({ playing, duration, onSeek, handleMouseUp, player }) => {
+
+  const [position, setPosition] = useState(0)
+
+  let getPlayerStateTimer = null;
+
+  const getPlayerCurrentstate = () => {
+    player.getCurrentState()
+    .then((state) => {
+      setPosition(state.position)
+    });
+  }  
+
+  // const setTimer = useCallback( () => {
+  //   playing ? getPlayerStateTimer = setInterval( getPlayerCurrentstate(), 1000) : clearInterval(getPlayerStateTimer);
+  // }, [playing]) 
+
+  useEffect(() => {
+    playing ? getPlayerStateTimer = setInterval(getPlayerCurrentstate(), 1000) : clearInterval(getPlayerStateTimer);
+     
+    return () => {
+      clearInterval(getPlayerStateTimer);
+    };
+  });
+
+
+
   return (
     <div className="Track-Progression" >
-      {children}
+      <Time ms={position} />
+      <div className="Track-Bar">
+        <ProgressionBar currentPostion={position} sliderAction={onSeek} maxValue={duration} handleMouseUp={handleMouseUp} />
+      </div>
+      <Time ms={duration} />
     </div>
   );
 }
