9f1617a61c6f2d4532be3fe34bb89ebd0886d57b
RCVZ <robinvz17@gmail.com>
2019-01-19 17:06:40 +0100

    clean up

--- src/Containers/PlaylistDisplay/PlaylistDisplay.js
+++ src/Containers/PlaylistDisplay/PlaylistDisplay.js
@@ -3,16 +3,17 @@ import './PlaylistDisplay.css';
 
 import Card from '../../Components/Card/Card';
 
-const PlaylistDisplay = ({ playlists, traverse, albums, libary, history, istrackList, handleOnAdd }) => {
+const PlaylistDisplay = ({ playlists, traverse, albums, libary, history, istrackList, addToNewPlaylist, addToCurrentPlaylist }) => {
   return (
     <div className="PlaylistDisplay">
       {playlists.map(playlist => {
         return (
           <Card
+            addToCurrentPlaylist={addToCurrentPlaylist}
+            addToNewPlaylist={addToNewPlaylist}
             playlist={playlist}
             id={playlist.id}
             key={playlist.id}
-            handleOnAdd={handleOnAdd}
             history={history}
             istrackList={istrackList}
           />)
