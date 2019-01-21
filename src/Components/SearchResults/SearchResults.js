a1d76ca2d9178bb1a08aac61c7031d0f193fe9ed
RCVZ <robinvz17@gmail.com>
2019-01-21 22:11:08 +0100

    added noresults

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -3,17 +3,31 @@ import './SearchResults.css';
 
 import Grid from '../Grid/Grid';
 
+import ResultsTracklist from '../../Containers/ResultsTracklist/ResultsTracklist';
+import Albumslist from '../../Containers/Albumslist/Albumslist';
+import Artists from '../../Containers/Artists/Artists';
+import Playlists from '../../Containers/Playlists/Playlists';
+
 import { Switch, Route } from "react-router-dom";
+
 
 const SearchResults = ({ children }) => {
   return (
     <Grid>
       <Switch>
-        <Route exact path="/search" render={() => children} />
-        <Route path="/search/playlists" render={() => children[0]} />
-        <Route path="/search/tracks" render={() => children[1]} />
-        <Route path="/search/albums" render={() => children[2]} />
-        <Route path="/search/artists" render={() => children[3]} />
+        <Route exact path="/search" render={(props) => (
+          <>
+          <Playlists key={1} {...props} />
+          <ResultsTracklist key={2} {...props} />
+          <Albumslist key={3} {...props} />
+          <Artists key={4}  {...props} />
+          </>)} 
+        />
+        <Route path="/search/no-results" render={() => <h1 style={{fontSize: '30px', marginTop: '30px'}}>No results</h1>} />
+        <Route path="/search/playlists" render={(props) => <Playlists key={1} {...props} /> } />
+        <Route path="/search/tracks" render={(props) => <ResultsTracklist key={2} {...props} /> } />
+        <Route path="/search/albums" render={(props) => <Albumslist key={3} {...props} /> } />
+        <Route path="/search/artists" render={(props) => <Artists key={4}  {...props} /> } />
       </Switch>
     </Grid>
   );
@@ -21,3 +35,9 @@ const SearchResults = ({ children }) => {
 
 
 export default SearchResults;
+
+
+
+  
+  
+  
