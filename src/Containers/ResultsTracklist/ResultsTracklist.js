37eafc821d1868986b5594910407d569f5cadcc3
RCVZ <robinvz17@gmail.com>
2019-01-26 23:54:39 +0100

    implanted virtual scroll list

--- src/Containers/ResultsTracklist/ResultsTracklist.js
+++ src/Containers/ResultsTracklist/ResultsTracklist.js
@@ -13,21 +13,50 @@ const ResultsTracklist = ({ history }) => {
   const [expand, toggleExpand] = useState(() => ({
     expanded: false,
     state: 'More',
-    results: 4 
+    start: 0,
+    end: 4 
   }));
 
+  const [scroll, updateScroll] = useState(() => ({
+    scrollTopPosition: 100,
+    prevScrollPosition: 0
+  }))
+
   const handleToggleExpand = () => {
-    history.location.pathname === '/search/tracks' ? history.push('/search') : history.push('/search/tracks');  
+    if (history.location.pathname === '/search/tracks' ) {
+      history.push('/search');
+      toggleExpand({ expanded: false, state: 'More', start: 0, end: 4 });
+    } else {       
+      history.push('/search/tracks');
+      toggleExpand({ expanded: true, state: 'Less', start: 0, end: 8 });
+    }  
   }
 
-  useEffect(
-    () => {
-      history.location.pathname === '/search' ?
-        toggleExpand({ expanded: false, state: 'More', results: 4 })
-      :
-        toggleExpand({ expanded: true, state: 'Less', results: Infinity });
-    }, [history.location.pathname]
-  )
+  // useEffect(
+  //   () => {
+  //     history.location.pathname === '/search' ?
+  //       toggleExpand({ expanded: false, state: 'More', results: 4 })
+  //     :
+  //       toggleExpand({ expanded: true, state: 'Less', results: Infinity });
+  //   }, [history.location.pathname]
+  // )
+
+  const scrollPosition = (e) => {
+    if (e.target.scrollTop >= scroll.scrollTopPosition) {
+      toggleExpand({ expanded: true, state: 'Less', start: expand.start + 1, end: expand.end + 1 });
+      updateScroll({
+        scrollTopPosition: scroll.scrollTopPosition + 100,
+        prevScrollPosition: e.target.scrollTop
+      })
+    } else if ((e.target.scrollTop + 100) <= scroll.scrollTopPosition ) {
+      toggleExpand({ expanded: true, state: 'Less', start: expand.start - 1, end: expand.end - 1 });
+      updateScroll({
+        scrollTopPosition: scroll.scrollTopPosition - 100,
+        prevScrollPosition: e.target.scrollTop
+      })
+    }
+    console.log(e.target.scrollTop)
+  }
 
   const loadOnScroll = (e) => {
     console.log('test')
@@ -36,7 +65,16 @@ const ResultsTracklist = ({ history }) => {
   return (
     <div className="ResultsTracklist" onScroll={loadOnScroll}>
       <Header name={expand.state} buttonAction={handleToggleExpand}>Tracks</Header>
-      <TrackList trackAction={context.addToNewPlaylist} tracklist={context.tracks.slice(0, expand.results)}  />
+      <div className="viewport" onScroll={scrollPosition}>
+        <div className="list" style={{ height: context.tracks.length * 100}}>
+          <TrackList 
+            trackAction={context.addToNewPlaylist} 
+            tracklist={context.tracks} 
+            start={expand.start}
+            end={expand.end}
+          />
+        </div>
+      </div>
     </div>
   );
 }
