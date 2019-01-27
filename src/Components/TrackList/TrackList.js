c2f12b7fc45eda870560a3a6d0dfd0710eac4668
RCVZ <robinvz17@gmail.com>
2019-01-27 20:56:04 +0100

    restructuring

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -2,31 +2,35 @@ import React from 'react';
 import './TrackList.css'
 
 import Track from '../Track/Track';
-import Grid from '../Grid/Grid';
+//import Grid from '../Grid/Grid';
 
-const TrackList = ({ trackAction, inPlaylist, tracklist }) => {
+const TrackList = ({ trackAction, inPlaylist, tracklist, start, end }) => {
 
- const getUrisList = () => {
-   const newList = [];
-   tracklist.map((track) => newList.push(track.uri));
-   return [newList, tracklist];
- }
+  const getUrisList = () => {
+    const newList = [];
+    tracklist.map((track) => newList.push(track.uri));
+    return [newList, tracklist];
+  }
 
   return (
-    <Grid>
-      {tracklist.map((track) => {
-        return (
-          <Track
-            track={track}
-            key={track.id}
-            trackAction={trackAction}
-            inPlaylist={inPlaylist}
-            getUrisList={getUrisList}
-          />
-        )
+    <>
+      {/* eslint-disable-next-line array-callback-return */}
+      {tracklist.map((track, index) => { 
+        if (index >= start && index <= end) {
+          return (
+            <Track
+              index={index}
+              track={track}
+              key={track.id}
+              trackAction={trackAction}
+              inPlaylist={inPlaylist}
+              getUrisList={getUrisList}
+            />
+          )
+        }
       })
       }
-    </Grid>
+    </>
   );
 }
 
