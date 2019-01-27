a77bd990ba6d400d2091bb8020228149dabf6add
RCVZ <robinvz17@gmail.com>
2019-01-27 20:54:49 +0100

    function for track position within virtual list

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -3,7 +3,7 @@ import SpotifyApi from '../../util/Spotify';
 import './Track.css'
 
 import Text from '../Text/Text';
-import Time from '../Time/Time'; 
+import Time from '../Time/Time';
 import ActionOverlay from '../ActionOverlay/ActionOverlay';
 
 import { ContextStore } from '../../Context/MainContext';
@@ -11,8 +11,8 @@ import { ContextStore } from '../../Context/MainContext';
 class Track extends PureComponent {
 
   static contextType = ContextStore;
-  
-  handlePlay = (e) => {    
+
+  handlePlay = (e) => {
     const [urisList, tracklist] = this.props.getUrisList();
     SpotifyApi.playTrack(this.props.track.uri, urisList);
     this.context.addToCurrentPlaylist(tracklist)
@@ -22,28 +22,39 @@ class Track extends PureComponent {
     this.props.trackAction(this.props.track)
   }
 
+  getPosition = () => {
+    const { index } = this.props
+    if (index % 2 === 0) {
+      let position = index * 50
+      return { top: position, left: 0}
+    } else {
+      let position = (index - 1) * 50
+      return { top: position, right: 0}
+    }
+  }
+
   render() {
-    const {artists, name, album, duration_ms } = this.props.track;
-    return(
-      <div className="Track">
+    const { artists, name, album, duration_ms } = this.props.track;
+    return (
+      <div className="Track" style={ this.getPosition() }>
         <div className="Track-information">
           <div className="Album-img" >
-            {album.images[2] ? 
+            {album.images[2] ?
               <img className="shadow" src={album.images[2].url} alt="album" /> :
               null
-            }            
-          </div> 
+            }
+          </div>
           <div className="Track-name">
-            <Text 
-              name={name} 
-              artist={artists[0].name} 
-              shadow={'0 15px 40px 1px rgba(0,0,0,0.30)' }
+            <Text
+              name={name}
+              artist={artists[0].name}
+              shadow={'0 15px 40px 1px rgba(0,0,0,0.30)'}
             />
-          </div>  
-          <Time className="shadow" ms={duration_ms} />          
-        </div>    
-        <ActionOverlay trackAction={this.handleTrackAction} onPlayClick={this.handlePlay} inPlaylist={this.props.inPlaylist}/>   
-      </div> 
+          </div>
+          <Time className="shadow" ms={duration_ms} />
+        </div>
+        <ActionOverlay trackAction={this.handleTrackAction} onPlayClick={this.handlePlay} inPlaylist={this.props.inPlaylist} />
+      </div>
     );
   }
 }
