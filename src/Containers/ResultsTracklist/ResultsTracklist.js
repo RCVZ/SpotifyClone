2bd5e66cae29d188de1afa0337a66954ea6d3cf8
RCVZ <robinvz17@gmail.com>
2019-01-27 20:57:17 +0100

    added virtual scroll list

--- src/Containers/ResultsTracklist/ResultsTracklist.js
+++ src/Containers/ResultsTracklist/ResultsTracklist.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 import './ResultsTracklist.css';
 
 import TrackList from '../../Components/TrackList/TrackList';
@@ -13,26 +13,90 @@ const ResultsTracklist = ({ history }) => {
   const [expand, toggleExpand] = useState(() => ({
     expanded: false,
     state: 'More',
-    results: 4 
+    height: 200
+  }));
+
+  const [visibleItems, updateVisibility] = useState(() => ({
+    start: 0,
+    end: 5
+  }));
+
+  const [scroll, updateScroll] = useState(() => ({
+    scrollTopPosition: 100,
+    prevScrollPosition: 0
   }));
 
   const handleToggleExpand = () => {
-    history.location.pathname === '/search/tracks' ? history.push('/search') : history.push('/search/tracks');  
+    if (history.location.pathname === '/search/tracks') {
+      history.push('/search');
+      toggleExpand({
+        expanded: false,
+        state: 'More',
+        height: 350
+      });
+      updateVisibility({
+        start: 0,
+        end: 5
+      });
+    } else {
+      history.push('/search/tracks');
+      toggleExpand({
+        expanded: true,
+        state: 'Less',
+        height: (context.tracks.length - 1) * 50
+      });
+      updateVisibility({
+        start: 0,
+        end: 18
+      });
+    }
+  };
+
+  const scrollPosition = (e) => {
+    if (!expand.expanded) return
+
+    if (e.target.scrollTop >= scroll.scrollTopPosition) {
+      updateVisibility({
+        start: visibleItems.start + 2,
+        end: visibleItems.end + 2
+      });
+      updateScroll({
+        scrollTopPosition: scroll.scrollTopPosition + 100,
+        prevScrollPosition: e.target.scrollTop
+      });
+    }
+
+    else if ((e.target.scrollTop + 100) <= scroll.scrollTopPosition) {
+      updateVisibility({
+        start: visibleItems.start - 2,
+        end: visibleItems.end - 2
+      });
+      updateScroll({
+        scrollTopPosition: scroll.scrollTopPosition - 100,
+        prevScrollPosition: e.target.scrollTop
+      });
+    }
+    console.log(scroll.scrollTopPosition)
+    console.log(e.target.scrollTop)
   }
 
-  useEffect(
-    () => {
-      history.location.pathname === '/search' ?
-        toggleExpand({ expanded: false, state: 'More', results: 4 })
-      :
-        toggleExpand({ expanded: true, state: 'Less', results: Infinity });
-    }, [history.location.pathname]
-  )
+  const loadOnScroll = (e) => {
+    console.log('test')
+  };
 
   return (
-    <div className="ResultsTracklist" >
+    <div className="ResultsTracklist" onScroll={loadOnScroll}>
       <Header name={expand.state} buttonAction={handleToggleExpand}>Tracks</Header>
-      <TrackList trackAction={context.addToNewPlaylist} tracklist={context.tracks.slice(0, expand.results)}  />
+      <div className="viewport" onScroll={scrollPosition} style={{ height: expand.expanded ? '650px' : '350px' }} >
+        <div className="list" style={{ height: expand.height }}>
+          <TrackList
+            trackAction={context.addToNewPlaylist}
+            tracklist={context.tracks}
+            start={visibleItems.start}
+            end={visibleItems.end}
+          />
+        </div>
+      </div>
     </div>
   );
 }
