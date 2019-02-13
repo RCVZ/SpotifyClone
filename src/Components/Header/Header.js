0989aa3ebae1ed50cc409712242d6b793802ab19
RCVZ <robinvz17@gmail.com>
2019-02-13 22:13:59 +0100

    performance update

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React,{ memo } from 'react';
 import './Header.css';
 import Title from '../Title/Title';
 import Button from '../Buttons/Button/Button';
 import Border from '../Border/Border';
 
-const Header = ({ children, buttonAction, name }) => {
+const areEqual = (prevProps, nextProps) => {
+  return true
+}
+
+const Header = memo(({ children, buttonAction, name }) => {
   return (
     <div className="Header" >
       <Title>{children}</Title>
       <Button type="button" buttonAction={buttonAction} name={name} />
       <Border />
     </div>
-  );
-}
+    )
+}, areEqual)
 
 export default Header;
