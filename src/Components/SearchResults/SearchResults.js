e43e49548e2175d755066d7ed75cdfcb681fe772
RCVZ <robinvz17@gmail.com>
2019-02-13 22:14:23 +0100

    change loadmore func

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -11,21 +11,21 @@ import Playlists from '../../Containers/Playlists/Playlists';
 import { Switch, Route } from "react-router-dom";
 
 
-const SearchResults = () => {
+const SearchResults = ({ loadMore }) => {
   return (
     <Grid>
       <Switch>
         <Route exact path="/search" render={(props) => (
           <>
-          <Playlists key={1} {...props} />
-          <ResultsTracklist key={2} {...props} />
-          <Albumslist key={3} {...props} />
-          <Artists key={4}  {...props} />
+            <Playlists key={1} {...props} />
+            <ResultsTracklist key={2} {...props} loadMore={loadMore}/>
+            <Albumslist key={3} {...props} />
+            <Artists key={4}  {...props} />
           </>)} 
         />
         <Route path="/search/no-results" render={() => <h1 style={{fontSize: '30px', marginTop: '30px'}}>No results</h1>} />
         <Route path="/search/playlists" render={(props) => <Playlists key={1} {...props} /> } />
-        <Route path="/search/tracks" render={(props) => <ResultsTracklist key={2} {...props} /> } />
+        <Route path="/search/tracks" render={(props) => <ResultsTracklist key={2} {...props} loadMore={loadMore}/> } />
         <Route path="/search/albums" render={(props) => <Albumslist key={3} {...props} /> } />
         <Route path="/search/artists" render={(props) => <Artists key={4}  {...props} /> } />
       </Switch>
