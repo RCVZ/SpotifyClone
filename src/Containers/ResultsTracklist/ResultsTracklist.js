d20068c6f01c6d1d1ee72269be2c9eed456c21e0
RCVZ <robinvz17@gmail.com>
2019-02-15 23:52:02 +0100

    perf test

--- src/Containers/ResultsTracklist/ResultsTracklist.js
+++ src/Containers/ResultsTracklist/ResultsTracklist.js
@@ -2,6 +2,7 @@ import React, { useState, useContext, useCallback } from 'react';
 import './ResultsTracklist.css';
 
 import TrackList from '../../Components/TrackList/TrackList';
+import Track from '../../Components/Track/Track';
 import Header from '../../Components/Header/Header';
 import Viewport from '../../Components/Viewport/Viewport';
 import { ContextStore } from '../../Context/MainContext';
@@ -14,6 +15,11 @@ const ResultsTracklist = ({ history, loadMore }) => {
   const [expanded, toggleExpand] = useState(() => false);
   const [scrollActive, activeScroll] = useState(() => false);
 
+  const [scroll, updateScroll] = useState({
+    offSet: 0,
+    direction: 'down'
+  });
+
   let timer = null;
 
   const handleToggleExpand = () => {
@@ -24,7 +30,7 @@ const ResultsTracklist = ({ history, loadMore }) => {
     } else {
       history.push('/search/tracks');
       toggleExpand(true);
-      updateVisibility({ start: 0,  end: 32 });
+      updateVisibility({ start: 0,  end: 20 });
     }
   };
 
@@ -52,39 +58,82 @@ const ResultsTracklist = ({ history, loadMore }) => {
 
     activeScroll(true);
     clearTimeout(timer)
-    timer = setTimeout(() => activeScroll(false), 66);
+    timer = setTimeout(() => activeScroll(false), 1000);
     updateVisibility({ start: visibleItems.start + 2, end: visibleItems.end + 2 });
 
-    if (context.tracks.length <= (visibleItems.end + 10)) {
+    if (context.tracks.length <= (visibleItems.end + 20)) {
       loadMore();
     }
   }
 
   const renderPrev = () => {
-    if (!expanded) return
+    if (!expanded || visibleItems.start === 0) return    
 
     activeScroll(true);
     clearTimeout(timer)
-    timer = setTimeout(() => activeScroll(false), 66);
+    timer = setTimeout(() => activeScroll(false), 1000);
     updateVisibility({ start: visibleItems.start - 2, end: visibleItems.end - 2 });
   }
 
+
+  const scrollPosition = (e) => {
+    scroll.offSet < e.target.scrollTop
+      ? updateScroll({
+        offSet: e.target.scrollTop,
+        direction: 'down'
+      })
+      : updateScroll({
+        offSet: e.target.scrollTop,
+        direction: 'up'
+      })
+
+    if (context.tracks.length <= (start + 26)) {
+      loadMore();
+    }
+    // if (e.target.scrollTop >= scrollTopPosition) {
+    //   renderNext()
+    //   updateScrollTopPosition(scrollTopPosition + 100);      
+    // }
+
+    // else if (e.target.scrollTop + 100 <= scrollTopPosition) {
+    //   renderPrev()
+    //   updateScrollTopPosition(scrollTopPosition - 100);      
+    // }
+
+  }
+
   const memoTracksWithPosition = useCallback(() => {
-    return tracksWithPosition(context.tracks);
+    let list = [...context.tracks];
+    list.forEach((element, index) => {
+      element['position'] = getPosition(index);
+    });
+    console.log('test')
+    return list
   }, [context.tracks]);
 
-  let test = memoTracksWithPosition().slice(visibleItems.start, visibleItems.end);
+  const start = Math.floor(scroll.offSet / 50);
+  console.log(start)
+
+  const test = memoTracksWithPosition().slice(start, start+20);
+  const listHeight = (context.tracks.length * 50 + 100) +'px';
+
+
   return (
     <div className="ResultsTracklist">
       <Header name={expanded ? 'Less' : 'More' } buttonAction={handleToggleExpand}>Tracks</Header>
-      <Viewport renderNext={renderNext} renderPrev={renderPrev} style={{ height: expanded ? '675px' : '350px' }} >
-        <div className="list" style={{ height: expanded ? (memoTracksWithPosition.length * 50 ) + 100 +'px' : '350px', pointerEvents: scrollActive ? 'none': 'auto' }}>
-          <TrackList
-            trackAction={context.addToNewPlaylist}
-            tracklist={test}
-            start={visibleItems.start}
-            end={visibleItems.end}
-          />
+      <Viewport scrollPosition={scrollPosition} renderNext={renderNext} renderPrev={renderPrev} style={{ height: expanded ? '700px' : '350px' }} >
+        <div className="list" style={{ height: listHeight, pointerEvents: scrollActive ? 'none': 'auto' }}>
+            {
+              test.map((track, index) => {
+                return (
+                  <Track
+                    index={index}
+                    track={track}
+                    key={track.id}
+                  />
+                )
+              })
+            }
         </div>
       </Viewport>
     </div>
