32a34093533b3a4fea480a15dd42e6bec1637f03
RCVZ <robinvz17@gmail.com>
2019-02-15 23:51:51 +0100

    perf test

--- src/Components/Viewport/Viewport.js
+++ src/Components/Viewport/Viewport.js
@@ -1,22 +1,33 @@
 import React, { useState } from 'react';
 import './Viewport.css';
 
-const Viewport = ({children, style, renderNext, renderPrev}) => {
-  const [scrollTopPosition, updateScrollTopPosition] = useState(() => 100);
+const Viewport = ({children, style, renderNext, renderPrev, scrollPosition}) => {
+  // const [scroll, updateScroll] = useState({
+  //   offSet: 0,
+  //   direction: 'down'
+  // });
 
-  const scrollPosition = (e) => {
+  // const scrollPosition = (e) => {
+  //   scroll.offSet < e.target.scrollTop 
+  //   ? updateScroll({
+  //     offSet: e.target.scrollTop ,
+  //     direction: 'down'
+  //   })
+  //   : updateScroll({
+  //     offSet: e.target.scrollTop ,
+  //       direction: 'up'
+  //     })
+  //   // if (e.target.scrollTop >= scrollTopPosition) {
+  //   //   renderNext()
+  //   //   updateScrollTopPosition(scrollTopPosition + 100);      
+  //   // }
 
-    if (e.target.scrollTop >= scrollTopPosition) {
-      renderNext()
-      updateScrollTopPosition(scrollTopPosition + 100);      
-    }
+  //   // else if (e.target.scrollTop + 100 <= scrollTopPosition) {
+  //   //   renderPrev()
+  //   //   updateScrollTopPosition(scrollTopPosition - 100);      
+  //   // }
 
-    else if ((e.target.scrollTop + 100) <= scrollTopPosition) {
-      renderPrev()
-      updateScrollTopPosition(scrollTopPosition - 100);      
-    }
-
-  }
+  // }
 
     return (
       <div className="Viewport" onScroll={scrollPosition} style={style} >
