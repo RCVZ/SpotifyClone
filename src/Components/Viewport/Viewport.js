cf2f6eac65ebfa0b55a0dc61704032a3294062e2
RCVZ <robinvz17@gmail.com>
2019-02-16 17:16:10 +0100

    clean up

--- src/Components/Viewport/Viewport.js
+++ src/Components/Viewport/Viewport.js
@@ -1,33 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Viewport.css';
 
-const Viewport = ({children, style, renderNext, renderPrev, scrollPosition}) => {
-  // const [scroll, updateScroll] = useState({
-  //   offSet: 0,
-  //   direction: 'down'
-  // });
-
-  // const scrollPosition = (e) => {
-  //   scroll.offSet < e.target.scrollTop 
-  //   ? updateScroll({
-  //     offSet: e.target.scrollTop ,
-  //     direction: 'down'
-  //   })
-  //   : updateScroll({
-  //     offSet: e.target.scrollTop ,
-  //       direction: 'up'
-  //     })
-  //   // if (e.target.scrollTop >= scrollTopPosition) {
-  //   //   renderNext()
-  //   //   updateScrollTopPosition(scrollTopPosition + 100);      
-  //   // }
-
-  //   // else if (e.target.scrollTop + 100 <= scrollTopPosition) {
-  //   //   renderPrev()
-  //   //   updateScrollTopPosition(scrollTopPosition - 100);      
-  //   // }
-
-  // }
+const Viewport = ({children, style, scrollPosition}) => {
 
     return (
       <div className="Viewport" onScroll={scrollPosition} style={style} >
