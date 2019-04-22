4818bb15437ffdf4122603d7cb9fe4458b0d9620
RCVZ <robinvz17@gmail.com>
2019-04-22 21:09:27 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent } from "react";
+import "./App.css";
 
-import './App.css';
-
-import SearchBar from './Containers/SearchBar/SearchBar';
-import Navigation from './Components/Navigation/Navigation';
-import NewPlaylist from './Containers/NewPlaylist/NewPlaylist';
-import CurrentPlaylist from './Components/CurrentPlaylist/CurrentPlaylist';
-import Main from './Containers/Main/Main';
-import Player from './Containers/Player/Player';
-import Library from './Containers/Library/Library';
-import UserPlaylists from './Containers/UserPlaylists/UserPlaylists';
-import SpotifyApi from './util/Spotify';
+import SearchBar from "./Containers/SearchBar/SearchBar";
+import Navigation from "./Components/Navigation/Navigation";
+import NewPlaylist from "./Containers/NewPlaylist/NewPlaylist";
+import CurrentPlaylist from "./Components/CurrentPlaylist/CurrentPlaylist";
+import Main from "./Containers/Main/Main";
+import Player from "./Containers/Player/Player";
+import Library from "./Containers/Library/Library";
+import UserPlaylists from "./Containers/UserPlaylists/UserPlaylists";
+import SpotifyApi from "./util/Spotify";
 
 import { withRouter } from "react-router-dom";
 
@@ -22,14 +21,14 @@ class App extends PureComponent {
   render() {
     return (
       <div className="App">
-        <SearchBar 
+        <SearchBar
           history={this.props.history} //temp  <----    workaround
-          location={this.props.location} //temp <----  
+          location={this.props.location} //temp <----
         />
         <Navigation />
         <Main
           history={this.props.history} //temp  <----    workaround
-          location={this.props.location} //temp <----  
+          location={this.props.location} //temp <----
         >
           <CurrentPlaylist />
           <NewPlaylist />
